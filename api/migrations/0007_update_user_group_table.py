408f5909e6b54f42c1517c4e6e1fc6f051beb016
Oleg Pikhnenko <opikhnenko@ronasit.com>
2020-11-10 20:47:03 +0600

    - fix migrations

--- api/migrations/0007_update_user_group_table.py
+++ api/migrations/0007_update_user_group_table.py
@@ -3,14 +3,10 @@
 from django.db import migrations, connection
 
 
-def update_user_table(*args, **kwargs):
+def update_user_group_table(*args, **kwargs):
     with connection.cursor() as cursor:
         cursor.execute(
-            'alter table "user" '
-                'ADD COLUMN is_superuser boolean not null default False, '
-                'ADD COLUMN is_staff boolean not null default True, '
-                'ADD COLUMN date_joined timestamp with time zone not null default now(), '
-                'ADD COLUMN last_login timestamp with time zone, '
+            'alter table "usergroup" '
                 'ADD COLUMN created_at timestamp with time zone not null default now(), '
                 'ADD COLUMN updated_at timestamp with time zone not null default now(), '
                 'ADD COLUMN deleted timestamp with time zone'
@@ -21,9 +17,9 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('api', '0005_set_user_auth_group'),
+        ('api', '0006_update_user_table'),
     ]
 
     operations = [
-        migrations.RunPython(update_user_table),
+        migrations.RunPython(update_user_group_table),
     ]
