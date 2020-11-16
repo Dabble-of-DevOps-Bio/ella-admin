340f3f4b3217ad389923e5422d7d4b5b8cf87906
apetrosyan <apetrosyan@ronasit.com>
2020-11-16 12:08:28 +0600

    add py config for stg

---
+++ ella-admin/settings/stg.py
@@ -0,0 +1,36 @@
+from .base import *
+
+ENV = 'staging'
+
+SECRET_KEY = '8g#mt#p!f^+xy-n6p+64n&cm92b61zi6_9q097(3e0o)xntbt_'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+FRONTEND_DOMAIN = 'localhost'
+FRONTEND_URL = 'http://%s' % FRONTEND_DOMAIN
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ella_api',
+        'USER': 'ella_api',
+        'PASSWORD': 'evangelicalism620455',
+        'HOST': 'postgresql',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = 'static/'
+MEDIA_URL = f'http://localhost/media/'
+MEDIA_ROOT = 'media/'
+STATIC_ROOT = 'api/static'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.mailtrap.io'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+EMAIL_SUPPORT_EMAIL = 'ella-support@mail.com'
