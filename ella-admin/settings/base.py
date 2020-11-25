766c86df938de28768df0094d2df9ef6d6af338a
Oleg Pikhnenko <opikhnenko@ronasit.com>
2020-11-25 23:08:16 +0600

    update cors policy

--- ella-admin/settings/base.py
+++ ella-admin/settings/base.py
@@ -107,6 +107,8 @@ SWAGGER_SETTINGS = {
     'LOGOUT_URL': '/admin/logout'
 }
 
+CORS_ORIGIN_WHITELIST = ()
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = default_methods
 CORS_ALLOW_HEADERS = default_headers + (
     'access-control-expose-headers',
