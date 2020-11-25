ba366447c396ee8a1265a96548ab5a176a06eb12
Oleg Pikhnenko <opikhnenko@ronasit.com>
2020-11-25 22:02:20 +0600

    update cross login

--- api/tests/user_session.py
+++ api/tests/user_session.py
@@ -1,5 +1,6 @@
 from api.tests.test import TestCase
 from api.models import UserSession
+from rest_framework import status
 
 
 class UserSessionTest(TestCase):
@@ -9,12 +10,11 @@ class UserSessionTest(TestCase):
         self.force_login_user(1)
         response = self.client.post('/api/staff-app-login/')
 
-        self.assertOk(response)
+        self.assertEquals(response.status_code, status.HTTP_302_FOUND)
 
         self.assertEqual(UserSession.objects.all().count(), 1)
 
         self.assertIsNotNone(response.cookies['AuthenticationToken'])
         self.assertEqual(response.cookies['AuthenticationToken']['path'], '/')
-        self.assertEqual(response.cookies['AuthenticationToken']['domain'], 'localhost')
         self.assertTrue(response.cookies['AuthenticationToken']['httponly'])
         self.assertIsNotNone(response.cookies['AuthenticationToken']['expires'])
