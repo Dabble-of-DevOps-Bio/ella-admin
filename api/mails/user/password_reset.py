0556671bb2403b54703ea5da514917759a57b44d
Oleg Pikhnenko <opikhnenko@ronasit.com>
2020-11-26 15:25:48 +0600

    - update email templates

--- api/mails/user/password_reset.py
+++ api/mails/user/password_reset.py
@@ -6,5 +6,5 @@ from api.mails import Mail
 
 class PasswordReset(Mail):
     template_name = 'emails/user/password_reset.html'
-    subject = _('Reset your MBuild password')
+    subject = _('Reset your Ella password')
     from_email = settings.EMAIL_SUPPORT_EMAIL
